17f359b1041d689d614b1c23e3a32d28ab63b692
hasna pasha <38372270+hasanpasha@users.noreply.github.com>
2023-09-16 17:45:07 +0300

    add format to logging config

--- src/anunnaki/__main__.py
+++ src/anunnaki/__main__.py
@@ -1,9 +1,10 @@
-
 from anunnaki.main_app import MainApp
 
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                    datefmt='%Y-%m-%d:%H:%M:%S',
+                    level=logging.DEBUG)
 
 if __name__ == '__main__':
     app = MainApp()
