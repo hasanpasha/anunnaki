7518a67049dd44ab65bc4ec49164c9d4c0e93f93
hasna pasha <38372270+hasanpasha@users.noreply.github.com>
2023-09-26 22:23:50 +0300

    redesign media_view ui

--- src/anunnaki/view/media_ui.py
+++ src/anunnaki/view/media_ui.py
@@ -15,8 +15,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFormLayout, QFrame, QGridLayout,
-    QHBoxLayout, QLabel, QPushButton, QSizePolicy,
+from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QHBoxLayout,
+    QLabel, QPushButton, QScrollArea, QSizePolicy,
     QSpacerItem, QStackedWidget, QTabWidget, QVBoxLayout,
     QWidget)
 
@@ -24,7 +24,7 @@ class Ui_media_widget(object):
     def setupUi(self, media_widget):
         if not media_widget.objectName():
             media_widget.setObjectName(u"media_widget")
-        media_widget.resize(760, 526)
+        media_widget.resize(1247, 915)
         self.verticalLayout_2 = QVBoxLayout(media_widget)
         self.verticalLayout_2.setSpacing(0)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
@@ -35,15 +35,39 @@ class Ui_media_widget(object):
         self.media_main.setObjectName(u"media_main")
         self.gridLayout_2 = QGridLayout(self.media_main)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.media_main_layout = QGridLayout()
-        self.media_main_layout.setObjectName(u"media_main_layout")
-        self.poster = QLabel(self.media_main)
-        self.poster.setObjectName(u"poster")
-        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.scrollArea = QScrollArea(self.media_main)
+        self.scrollArea.setObjectName(u"scrollArea")
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollAreaWidgetContents_3 = QWidget()
+        self.scrollAreaWidgetContents_3.setObjectName(u"scrollAreaWidgetContents_3")
+        self.scrollAreaWidgetContents_3.setGeometry(QRect(0, 0, 1227, 895))
+        self.verticalLayout_9 = QVBoxLayout(self.scrollAreaWidgetContents_3)
+        self.verticalLayout_9.setObjectName(u"verticalLayout_9")
+        self.player_stack = QStackedWidget(self.scrollAreaWidgetContents_3)
+        self.player_stack.setObjectName(u"player_stack")
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.poster.sizePolicy().hasHeightForWidth())
-        self.poster.setSizePolicy(sizePolicy)
+        sizePolicy.setHeightForWidth(self.player_stack.sizePolicy().hasHeightForWidth())
+        self.player_stack.setSizePolicy(sizePolicy)
+        self.player_stack.setMinimumSize(QSize(640, 480))
+        self.player_stack.setFrameShape(QFrame.WinPanel)
+
+        self.verticalLayout_9.addWidget(self.player_stack)
+
+        self.verticalLayout_10 = QVBoxLayout()
+        self.verticalLayout_10.setObjectName(u"verticalLayout_10")
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.poster = QLabel(self.scrollAreaWidgetContents_3)
+        self.poster.setObjectName(u"poster")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.poster.sizePolicy().hasHeightForWidth())
+        self.poster.setSizePolicy(sizePolicy1)
         self.poster.setMinimumSize(QSize(210, 300))
         self.poster.setFrameShape(QFrame.Panel)
         self.poster.setFrameShadow(QFrame.Raised)
@@ -51,48 +75,82 @@ class Ui_media_widget(object):
         self.poster.setScaledContents(True)
         self.poster.setTextInteractionFlags(Qt.NoTextInteraction)
 
-        self.media_main_layout.addWidget(self.poster, 0, 0, 1, 1)
+        self.verticalLayout.addWidget(self.poster)
 
-        self.seasons_tabs = QTabWidget(self.media_main)
+        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer_2)
+
+
+        self.horizontalLayout_2.addLayout(self.verticalLayout)
+
+        self.verticalLayout_11 = QVBoxLayout()
+        self.verticalLayout_11.setObjectName(u"verticalLayout_11")
+        self.title_lbl = QLabel(self.scrollAreaWidgetContents_3)
+        self.title_lbl.setObjectName(u"title_lbl")
+        font = QFont()
+        font.setPointSize(18)
+        self.title_lbl.setFont(font)
+        self.title_lbl.setAlignment(Qt.AlignCenter)
+        self.title_lbl.setWordWrap(True)
+        self.title_lbl.setTextInteractionFlags(Qt.LinksAccessibleByMouse|Qt.TextSelectableByMouse)
+
+        self.verticalLayout_11.addWidget(self.title_lbl)
+
+        self.story_lbl = QLabel(self.scrollAreaWidgetContents_3)
+        self.story_lbl.setObjectName(u"story_lbl")
+        self.story_lbl.setWordWrap(True)
+        self.story_lbl.setTextInteractionFlags(Qt.TextBrowserInteraction)
+
+        self.verticalLayout_11.addWidget(self.story_lbl)
+
+        self.tags_lbl = QLabel(self.scrollAreaWidgetContents_3)
+        self.tags_lbl.setObjectName(u"tags_lbl")
+
+        self.verticalLayout_11.addWidget(self.tags_lbl)
+
+        self.play_btn = QPushButton(self.scrollAreaWidgetContents_3)
+        self.play_btn.setObjectName(u"play_btn")
+        self.play_btn.setEnabled(True)
+        sizePolicy1.setHeightForWidth(self.play_btn.sizePolicy().hasHeightForWidth())
+        self.play_btn.setSizePolicy(sizePolicy1)
+        self.play_btn.setMinimumSize(QSize(200, 50))
+        self.play_btn.setMaximumSize(QSize(200, 50))
+        font1 = QFont()
+        font1.setPointSize(16)
+        self.play_btn.setFont(font1)
+        self.play_btn.setAutoDefault(True)
+        self.play_btn.setFlat(False)
+
+        self.verticalLayout_11.addWidget(self.play_btn)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Preferred)
+
+        self.verticalLayout_11.addItem(self.verticalSpacer)
+
+
+        self.horizontalLayout_2.addLayout(self.verticalLayout_11)
+
+        self.seasons_tabs = QTabWidget(self.scrollAreaWidgetContents_3)
         self.seasons_tabs.setObjectName(u"seasons_tabs")
 
-        self.media_main_layout.addWidget(self.seasons_tabs, 0, 1, 1, 1)
+        self.horizontalLayout_2.addWidget(self.seasons_tabs)
 
-        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        self.horizontalSpacer_6 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
-        self.media_main_layout.addItem(self.verticalSpacer, 5, 0, 1, 1)
-
-        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
-
-        self.media_main_layout.addItem(self.horizontalSpacer, 4, 1, 1, 1)
-
-        self.info_layout = QFormLayout()
-        self.info_layout.setObjectName(u"info_layout")
-
-        self.media_main_layout.addLayout(self.info_layout, 4, 0, 1, 1)
-
-        self.horizontalLayout = QHBoxLayout()
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.play_btn = QPushButton(self.media_main)
-        self.play_btn.setObjectName(u"play_btn")
-        self.play_btn.setEnabled(False)
-
-        self.horizontalLayout.addWidget(self.play_btn)
+        self.horizontalLayout_2.addItem(self.horizontalSpacer_6)
 
 
-        self.media_main_layout.addLayout(self.horizontalLayout, 4, 2, 1, 1)
+        self.verticalLayout_10.addLayout(self.horizontalLayout_2)
 
 
-        self.gridLayout_2.addLayout(self.media_main_layout, 0, 0, 1, 1)
+        self.verticalLayout_9.addLayout(self.verticalLayout_10)
+
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents_3)
+
+        self.gridLayout_2.addWidget(self.scrollArea, 0, 0, 1, 1)
 
         self.media_stack.addWidget(self.media_main)
-        self.media_player = QWidget()
-        self.media_player.setObjectName(u"media_player")
-        self.verticalLayout_3 = QVBoxLayout(self.media_player)
-        self.verticalLayout_3.setSpacing(0)
-        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
-        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
-        self.media_stack.addWidget(self.media_player)
 
         self.verticalLayout_2.addWidget(self.media_stack)
 
@@ -100,6 +158,7 @@ class Ui_media_widget(object):
         self.retranslateUi(media_widget)
 
         self.media_stack.setCurrentIndex(0)
+        self.play_btn.setDefault(False)
 
 
         QMetaObject.connectSlotsByName(media_widget)
@@ -107,6 +166,12 @@ class Ui_media_widget(object):
 
     def retranslateUi(self, media_widget):
         media_widget.setWindowTitle(QCoreApplication.translate("media_widget", u"Form", None))
-        self.play_btn.setText(QCoreApplication.translate("media_widget", u"Play", None))
+        self.title_lbl.setText("")
+        self.story_lbl.setText("")
+        self.tags_lbl.setText("")
+        self.play_btn.setText(QCoreApplication.translate("media_widget", u"play", None))
+#if QT_CONFIG(shortcut)
+        self.play_btn.setShortcut(QCoreApplication.translate("media_widget", u"Ctrl+S", None))
+#endif // QT_CONFIG(shortcut)
     # retranslateUi
 
--- src/anunnaki/view/media_view.py
+++ src/anunnaki/view/media_view.py
@@ -1,7 +1,7 @@
 import logging
 
 from PySide6.QtWidgets import (
-    QWidget, QLabel, QListWidget, QListWidgetItem, QHBoxLayout
+    QWidget, QLabel, QListWidget, QListWidgetItem, QHBoxLayout, QListView
 )
 from PySide6.QtGui import QPixmap
 from PySide6.QtCore import QByteArray, Qt
@@ -24,14 +24,17 @@ class MediaView(QWidget):
 
         self.__ui = Ui_media_widget()
         self.__ui.setupUi(self)
+
+        self.__ui.player_stack.setVisible(False)
+        
         self.player = MPVWidget(DefaultController, self)
-        self.__ui.media_player.layout().addWidget(self.player)
+        self.player.initialized.connect(self.on_player_initialized)
         self.player.fullscreen_changed.connect(self.on_player_fullscreen_changed)
+
+        self.__ui.play_btn.clicked.connect(self.start_playing)
 
         self.__controller = controller
         self.__model = model
-
-        self.__ui.play_btn.clicked.connect(self.on_play_btn_clicked)
         
         self.__model.ready.connect(self.on_ready)
         self.__model.detail_loaded.connect(self.on_media_detail_loaded)
@@ -40,18 +43,29 @@ class MediaView(QWidget):
         self.__model.videos_and_subtitles_loaded.connect(self.on_videos_and_subtitles_loaded)
         self.__model.current_episode_changed.connect(self.on_current_episode_changed)
 
+    def on_player_initialized(self):
+        self.__ui.player_stack.addWidget(self.player)
+        self.__ui.player_stack.setCurrentWidget(self.player)
+    
     def on_player_fullscreen_changed(self, state: bool):
         if state:
+            print("showing fullscreen")
+            self.__ui.player_stack.removeWidget(self.player)
+            self.__ui.media_stack.addWidget(self.player)
+            self.__ui.media_stack.setCurrentWidget(self.player)
             self.main_window.menuBar().hide()
             self.main_window.statusBar().hide()
             self.main_window.showFullScreen()
         else:
+            self.__ui.media_stack.removeWidget(self.player)
+            self.__ui.media_stack.setCurrentWidget(self.__ui.media_main)
+            self.__ui.player_stack.addWidget(self.player)
+            self.__ui.player_stack.setCurrentWidget(self.player)
             self.main_window.menuBar().show()
             self.main_window.statusBar().show()
             self.main_window.showNormal()
 
     def on_current_episode_changed(self, episode: Episode):
-        self.__ui.play_btn.setEnabled(True)
         self.__controller.load_videos()
         self.__controller.load_subtitles()
 
@@ -59,10 +73,10 @@ class MediaView(QWidget):
         self.videos = videos
         self.subtitles = subtitles
         
-    def on_play_btn_clicked(self):
+    def start_playing(self):
         if self.videos is not None:
-            self.__ui.media_stack.setCurrentWidget(self.__ui.media_player)
             self.player.play(self.videos, self.subtitles)
+            self.__ui.player_stack.setVisible(True)
 
     def on_ready(self):
         self.__controller.load_detail()
@@ -74,12 +88,15 @@ class MediaView(QWidget):
                                                  Qt.AspectRatioMode.KeepAspectRatio))
 
     def on_episode_changed(self, row: int):
+        self.videos = None
+        self.subtitles = None
         season = self.__ui.seasons_tabs.currentIndex()
         self.__controller.set_current_episode_playing(season, row)
 
     def on_seasons_loaded(self, seasons: list[Season]):
         for season in seasons:
             tab = QListWidget()
+            tab.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
             tab.currentRowChanged.connect(self.on_episode_changed)
             for epi_indx, episode in enumerate(season.episodes):
                 item = QListWidgetItem(str(epi_indx))
@@ -92,18 +109,9 @@ class MediaView(QWidget):
 
         if media.thumbnail_url:
             self.__controller.load_poster(media.thumbnail_url)
-        self.__ui.info_layout.addRow("title", QLabel(media.title if media.title else "N/A"))
-        self.__ui.info_layout.addRow("kind", QLabel(media.kind.value if media.kind else "N/A"))
-        self.__ui.info_layout.addRow("year", QLabel(media.year if media.year else "N/A"))
-        desc_label = QLabel(media.description if media.description else "N/A")
-        desc_label.setWordWrap(True)
-        self.__ui.info_layout.addRow("description", desc_label)
-        if media.tags:
-            tags = QHBoxLayout()
-            for tag in media.tags:
-                tags.addWidget(QLabel(tag))
-            self.__ui.info_layout.addRow("tags", tags)
-
+        self.__ui.title_lbl.setText(f"{media.title} ({media.year})")
+        self.__ui.story_lbl.setText(media.description if media.description else "N/A")
+        self.__ui.tags_lbl.setText(' - '.join(media.tags))
         self.__controller.load_seasons()
 
     def open_media(self, media: Media, source: SourceBridge):
