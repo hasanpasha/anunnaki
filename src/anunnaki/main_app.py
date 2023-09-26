fc70e76569787c9df8c7bec5344674d90940c683
hasna pasha <38372270+hasanpasha@users.noreply.github.com>
2023-09-26 18:45:41 +0300

    set app name and version

--- src/anunnaki/main_app.py
+++ src/anunnaki/main_app.py
@@ -1,12 +1,15 @@
 from anunnaki.view.main_view import MainView
 from anunnaki.controller.main_ctrl import MainController
 from anunnaki.model.main_model import MainModel
+from anunnaki.constants import AppStrings
 
 from PySide6.QtWidgets import QApplication
 
 class MainApp(QApplication):
     def __init__(self):
         super(MainApp, self).__init__()
+        self.setApplicationName(AppStrings.APP_NAME)
+        self.setApplicationVersion(AppStrings.APP_VERSION)
         self.model = MainModel()
         self.controller = MainController(self.model)
         self.view = MainView(self.controller, self.model)
